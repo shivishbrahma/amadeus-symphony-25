97597ae32eb97c29154bdb34f16f4bf4d7c6403d
Purbayan Chowdhury <pur.cho.99@gmail.com>
2021-01-25 23:20:37 +0530

    Added New Service and Markdown support

--- cauldron_overflow/src/Controller/QuestionController.php
+++ cauldron_overflow/src/Controller/QuestionController.php
@@ -2,32 +2,44 @@
 
 namespace App\Controller;
 
+use App\Service\MarkdownHelper;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class QuestionController extends AbstractController
 {
     /**
      * @Route("/", name="app_home")
      */
-    public function homepage()
+    public function homepage(Environment $twigEnvironment)
     {
+        /*
+        // fun example of using the Twig service directly!
+        $html = $twigEnvironment->render('question/homepage.html.twig');
+
+        return new Response($html);
+        */
+
         return $this->render('question/homepage.html.twig');
     }
 
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug)
+    public function show($slug, MarkdownHelper $markdownHelper)
     {
         $answers = [
-            'Make sure your cat is sitting purrrfectly still 🤣',
+            'Make sure your cat is sitting `purrrfectly` still 🤣',
             'Honestly, I like furry shoes better than MY cat',
             'Maybe... try saying the spell backwards?',
         ];
-
+        $questionText =
+            'I\'ve been turned into a cat, any *thoughts* on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
+        $parsedQuestionText = $markdownHelper->parse($questionText);
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
+            'question_text' => $parsedQuestionText,
             'answers' => $answers,
         ]);
     }
